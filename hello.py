a11d80b54ea48d3f251936121e8da3f918d030d3
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-13 17:54:12 +0700

    Update

--- hello.py
+++ hello.py
@@ -7,7 +7,10 @@ destination = requests.get("https://webscraper.io/test-sites/e-commerce/allinone
 soup = BeautifulSoup(destination.content, 'html.parser')
 list = []
 for i in soup.findAll(class_ = "title"):
-    list.append(i.get("title"))
+    sub_list = []
+    sub_list.append(i.get("title"))
+    sub_list.append("https://webscraper.io"+i.get("href"))
+    list.append(sub_list)
 title = pd.DataFrame(list)
 print(title)
 
