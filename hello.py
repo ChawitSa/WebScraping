2e29feb6c276ffc10443ff7f7eeb27fa53daf666
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-13 13:09:12 +0700

    Update

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 from bs4 import  BeautifulSoup
-print(BeautifulSoup("<p>Some<b>bad<i>HTML").prettify())
+import requests
+import  pandas as pd
+
+
+destination = requests.get("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops")
+soup = BeautifulSoup(destination.content, 'html.parser')
+list = []
+for i in soup.findAll(class_ = "title"):
+    list.append(i.get("title"))
+title = pd.DataFrame(list)
+print(title)
+
